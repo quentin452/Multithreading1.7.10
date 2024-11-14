33b8241d1808dd4c63a40fd6e3530adfc1865f3c
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-11-14 21:22:18 +0000

    Call findChunksForSpawning every 4 ticks instead of 1

--- src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/MixinWorldServer.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/core/MixinWorldServer.java
@@ -7,6 +7,7 @@ import java.util.*;
 
 import net.minecraft.block.Block;
 import net.minecraft.entity.EntityTracker;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.network.play.server.S2BPacketChangeGameState;
 import net.minecraft.profiler.Profiler;
 import net.minecraft.scoreboard.ScoreboardSaveData;
@@ -36,8 +37,12 @@ public abstract class MixinWorldServer extends World {
 
     @Unique
     private WorldServer optimizationsAndTweaks$worldServer;
+    @Unique
+    private int optimizationsAndTweaks$tickCounter = 0;
     @Shadow
     private static final Logger logger = LogManager.getLogger();
+    @Shadow
+    public boolean allPlayersSleeping;
     @Shadow
     private final MinecraftServer mcServer;
     @Shadow
@@ -48,6 +53,10 @@ public abstract class MixinWorldServer extends World {
     private final Teleporter worldTeleporter;
     @Shadow
     private IntHashMap entityIdMap;
+    @Shadow
+    public final SpawnerAnimals animalSpawner = new SpawnerAnimals();
+    @Shadow
+    public List<Teleporter> customTeleporters = new ArrayList<>();
 
     public MixinWorldServer(MinecraftServer p_i45284_1_, ISaveHandler p_i45284_2_, String p_i45284_3_, int p_i45284_4_,
         WorldSettings p_i45284_5_, Profiler p_i45284_6_) {
@@ -261,5 +270,115 @@ public abstract class MixinWorldServer extends World {
         int p_147446_5_, int p_147446_6_) {
         WorldServerTwo
             .func_147446_b(this, p_147446_1_, p_147446_2_, p_147446_3_, p_147446_4_, p_147446_5_, p_147446_6_);
+    }
+    /**
+     * @author quentin452
+     * @reason call findChunksForSpawning every 4 ticks instead of 1
+     */
+    @Overwrite
+    public void tick()
+    {
+        super.tick();
+
+        if (this.getWorldInfo().isHardcoreModeEnabled() && this.difficultySetting != EnumDifficulty.HARD)
+        {
+            this.difficultySetting = EnumDifficulty.HARD;
+        }
+
+        this.provider.worldChunkMgr.cleanupCache();
+
+        if (this.areAllPlayersAsleep())
+        {
+            if (this.getGameRules().getGameRuleBooleanValue("doDaylightCycle"))
+            {
+                long i = this.worldInfo.getWorldTime() + 24000L;
+                this.worldInfo.setWorldTime(i - i % 24000L);
+            }
+
+            this.wakeAllPlayers();
+        }
+
+        this.theProfiler.startSection("mobSpawner");
+
+        if (this.getGameRules().getGameRuleBooleanValue("doMobSpawning")) {
+            if (optimizationsAndTweaks$tickCounter % 4 == 0) {
+                this.animalSpawner.findChunksForSpawning((WorldServer)(Object)this, this.spawnHostileMobs, this.spawnPeacefulMobs, this.worldInfo.getWorldTotalTime() % 400L == 0L);
+                optimizationsAndTweaks$tickCounter = 0;
+            }
+            optimizationsAndTweaks$tickCounter++;
+        }
+
+        this.theProfiler.endStartSection("chunkSource");
+        this.chunkProvider.unloadQueuedChunks();
+        int j = this.calculateSkylightSubtracted(1.0F);
+
+        if (j != this.skylightSubtracted)
+        {
+            this.skylightSubtracted = j;
+        }
+
+        this.worldInfo.incrementTotalWorldTime(this.worldInfo.getWorldTotalTime() + 1L);
+
+        if (this.getGameRules().getGameRuleBooleanValue("doDaylightCycle"))
+        {
+            this.worldInfo.setWorldTime(this.worldInfo.getWorldTime() + 1L);
+        }
+
+        this.theProfiler.endStartSection("tickPending");
+        this.tickUpdates(false);
+        this.theProfiler.endStartSection("tickBlocks");
+        this.func_147456_g();
+        this.theProfiler.endStartSection("chunkMap");
+        this.thePlayerManager.updatePlayerInstances();
+        this.theProfiler.endStartSection("village");
+        this.villageCollectionObj.tick();
+        this.villageSiegeObj.tick();
+        this.theProfiler.endStartSection("portalForcer");
+        this.worldTeleporter.removeStalePortalLocations(this.getTotalWorldTime());
+        for (Teleporter tele : customTeleporters)
+        {
+            tele.removeStalePortalLocations(getTotalWorldTime());
+        }
+        this.theProfiler.endSection();
+        this.func_147488_Z();
+    }
+    @Shadow
+    public boolean areAllPlayersAsleep()
+    {
+        if (this.allPlayersSleeping && !this.isRemote)
+        {
+            Iterator iterator = this.playerEntities.iterator();
+            EntityPlayer entityplayer;
+
+            do
+            {
+                if (!iterator.hasNext())
+                {
+                    return true;
+                }
+
+                entityplayer = (EntityPlayer)iterator.next();
+            }
+            while (entityplayer.isPlayerFullyAsleep());
+
+            return false;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    @Shadow
+    protected void wakeAllPlayers()
+    {
+    }
+    @Shadow
+    private void resetRainAndThunder()
+    {
+    }
+    @Shadow
+    private void func_147488_Z()
+    {
     }
 }
