154344cc37941224f18fa3ae2c30b80da31fb37e
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-11-16 15:13:04 +0000

    Remake MixinUtils

--- src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/etfuturumrequiem/MixinUtils.java
+++ src/main/java/fr/iamacat/optimizationsandtweaks/mixins/common/etfuturumrequiem/MixinUtils.java
@@ -2,78 +2,88 @@ package fr.iamacat.optimizationsandtweaks.mixins.common.etfuturumrequiem;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Unique;
 
 import ganymedes01.etfuturum.core.utils.Utils;
 
 @Mixin(Utils.class)
 public class MixinUtils {
 
-    /**
-     * @author
-     * @reason
-     */
-    @Overwrite
-    public static double atan2(double p_181159_0_, double p_181159_2_) {
-        double d0 = p_181159_2_ * p_181159_2_ + p_181159_0_ * p_181159_0_;
-        if (Double.isNaN(d0)) {
-            return Double.NaN;
-        } else {
-            boolean flag = p_181159_0_ < 0.0;
-            if (flag) {
-                p_181159_0_ = -p_181159_0_;
-            }
+    // Static constants to avoid resetting in each method call
+    @Unique
+    private static final double optimizationsAndTweaks$frac_bias = Double.longBitsToDouble(4805340802404319232L);
+    @Unique
+    private static final double[] optimizationsAndTweaks$asine_tab = new double[257];
+    @Unique
+    private static final double[] optimizationsAndTweaks$cos_tab = new double[257];
 
-            boolean flag1 = p_181159_2_ < 0.0;
-            if (flag1) {
-                p_181159_2_ = -p_181159_2_;
-            }
-
-            boolean flag2 = p_181159_0_ > p_181159_2_;
-            double d9;
-            if (flag2) {
-                d9 = p_181159_2_;
-                p_181159_2_ = p_181159_0_;
-                p_181159_0_ = d9;
-            }
-
-            d9 = fastInvSqrt(d0);
-            p_181159_2_ *= d9;
-            p_181159_0_ *= d9;
-
-            double d2 = p_181159_0_ + 0.5;
-            double d3 = Math.asin(d2);
-            double d4 = Math.cos(d3);
-            double d5 = p_181159_0_ * d4 - p_181159_2_;
-
-            double d6 = (6.0 + d5 * d5) * d5 * 0.16666666666666666;
-            double d7 = d3 + d6;
-            if (flag2) {
-                d7 = 1.5707963267948966 - d7;
-            }
-
-            if (flag1) {
-                d7 = Math.PI - d7;
-            }
-
-            if (flag) {
-                d7 = -d7;
-            }
-
-            return d7;
+    static {
+        // Initialize sine and cosine arrays once
+        for (int j = 0; j < 257; ++j) {
+            double ratio = (double) j / 256.0;
+            double asinValue = Math.asin(ratio);
+            optimizationsAndTweaks$cos_tab[j] = Math.cos(asinValue);
+            optimizationsAndTweaks$asine_tab[j] = asinValue;
         }
     }
 
     /**
-     * @author
-     * @reason
+     * @author quentin452
+     * @reason Optimize atan2
      */
     @Overwrite
-    public static double fastInvSqrt(double p_181161_0_) {
-        double d0 = 0.5 * p_181161_0_;
-        long i = Double.doubleToRawLongBits(p_181161_0_);
-        i = 6910469410427058090L - (i >> 1);
-        p_181161_0_ = Double.longBitsToDouble(i);
-        p_181161_0_ *= 1.5 - d0 * p_181161_0_ * p_181161_0_;
-        return p_181161_0_;
+    public static double atan2(double y, double x) {
+        // Check if any of the values ​​are NaN (Not a Number) and return NaN if so.
+        if (Double.isNaN(x) || Double.isNaN(y)) {
+            return Double.NaN;
+        }
+
+        // Handle cases where the values ​​are negative by reversing the signs if necessary
+        boolean isNegativeY = y < 0.0;
+        if (isNegativeY) y = -y;
+
+        boolean isNegativeX = x < 0.0;
+        if (isNegativeX) x = -x;
+
+        // Determine if y is greater than x
+        boolean isYGreaterThanX = y > x;
+        if (isYGreaterThanX) {
+            double temp = x;
+            x = y;
+            y = temp;
+        }
+
+        // Calculate magnitude and normalize components
+        double magnitudeSquared = x * x + y * y;
+        double invMagnitude = optimizationsAndTweaks$invSqrt(magnitudeSquared);
+        x *= invMagnitude;
+        y *= invMagnitude;
+
+        // Use the pre-calculated sine and cosine tables to find the angle
+        double angle = optimizationsAndTweaks$getAngle(y, x);
+
+        // Adjust angle based on initial signs
+        if (isYGreaterThanX) angle = (Math.PI / 2) - angle;
+        if (isNegativeX) angle = Math.PI - angle;
+        if (isNegativeY) angle = -angle;
+
+        return angle;
+    }
+
+    @Unique
+    private static double optimizationsAndTweaks$getAngle(double y, double x) {
+        double biasedY = optimizationsAndTweaks$frac_bias + y;
+        int index = (int) Double.doubleToRawLongBits(biasedY);
+        double asinValue = optimizationsAndTweaks$asine_tab[index];
+        double cosValue = optimizationsAndTweaks$cos_tab[index];
+        double deltaY = biasedY - optimizationsAndTweaks$frac_bias;
+        double delta = y * cosValue - x * deltaY;
+        double correctionTerm = (6.0 + delta * delta) * delta * 0.16666666666666666;
+        return asinValue + correctionTerm;
+    }
+
+    @Unique
+    private static double optimizationsAndTweaks$invSqrt(double num) {
+        return 1 / Math.sqrt(num);
     }
 }
